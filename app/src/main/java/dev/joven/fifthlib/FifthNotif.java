1ae5bfa379645d09b655b6d90bb421bbf3368e2b
Blast-Zone-Out <113412493+Blast-Zone-Out@users.noreply.github.com>
2023-12-06 13:35:00 +0800

    Finished

---
+++ app/src/main/java/dev/joven/fifthlib/FifthNotif.java
@@ -0,0 +1,20 @@
+package dev.joven.fifthlib;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class FifthNotif extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String title = intent.getStringExtra("title");
+        String message = intent.getStringExtra("message");
+
+        //Log.i("mgdNotification", "Received Notification: " + title + " - " + message);
+
+        Toast.makeText(context, "Received Notification: " + title + " - " + message, Toast.LENGTH_SHORT).show();
+    }
+}
